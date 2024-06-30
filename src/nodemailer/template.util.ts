a7e32789682451f76a93f7dcb0686ad4081ca1d6
jeth318 <jesper.thornberg@me.com>
2024-06-30 15:54:12 +0200

    Improve mail template

--- src/nodemailer/template.util.ts
+++ src/nodemailer/template.util.ts
@@ -13,7 +13,7 @@ const printReturnInfo = (ride: TransportData) =>
 
 const printCarInfo = (ride: TransportData) => ride.routes[0].carModel;
 
-export const style = `
+export const style = /*html*/ `
 <style>
     body {
         font-family: Arial, sans-serif;
@@ -23,6 +23,10 @@ export const style = `
         display: flex;
         flex-direction: column;
         align-items: center;
+    }
+
+    .preamble {
+      padding: 24px;
     }
 
     .container {
@@ -43,37 +47,74 @@ export const style = `
         margin-right: 4px;
     }
 
-    table {
+    .table-grid  {
         border-radius: 12px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         border-collapse: collapse;
+        display: flex;
+        flex-direction: column;
     }
 
-    th,
-    td {
+    .top-left-radius {
+      border-top-left-radius: 8px;
+    }
+
+    .top-right-radius {
+      border-top-right-radius: 8px;
+    }
+
+    .bottom-left-radius {
+      border-bottom-left-radius: 8px;
+    }
+
+    .bottom-right-radius {
+      border-bottom-right-radius: 8px;
+    }
+
+    .table-head,
+    .table-column {
         padding: 12px;
         text-align: left;
         border-bottom: 1px solid #ddd;
+        border-right: 1px solid #ddd;
     }
 
-    th {
+    .table-row {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .table-head {
         background-color: #fffb00;
         color: black;
         font-weight: bold;
+        min-width: 50px;
+        display: flex;
+        justify-content: left;
+        align-items: center;
     }
 
-    td {
+    .table-column{
         background-color: #fafafa;
+        width: 100%;
     }
-
-    tr:hover td {
+    .table-row:hover .table-column {
         background-color: #f1f1f1;
+    }
+    .flex-row-center {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      align-self: left;
+    }
+    .emoji {
+      margin-right: 10px;
     }
 </style>
 `;
 
 export const buildHtml = (ride: TransportData) => {
-  return `<html>
+  const doc = /*html*/ `<html>
   <head>
     ${style}
   </head>
@@ -81,21 +122,28 @@ export const buildHtml = (ride: TransportData) => {
     <div class="container">
       <div>
         <h1>🚗💨</h1>
-       
       </div> 
-       <p style="padding: 12px;">En ny gratisresa som matchar dina bevakningar har nyligen publicerats på Hertz Freerider. Ta en titt på den här.</p>
-      <table>
-        <tr>
-          <th>Pickup</th>
-          <td>➡️ ${ride.pickupLocationName}</td>
-        </tr>
-        <tr>
-          <th>Return</th>
-          <td>⬅️ ${ride.returnLocationName}</td>
-        </tr>
-        <tr>
-          <th>When</th>
-          <td>
+       <p class="preamble">En ny gratisresa som matchar dina bevakningar har nyligen publicerats på Hertz Freerider. Ta en titt på den här.</p>
+      <div class="table-grid">
+        <div class="table-row">
+          <div class="table-head">Pickup</div>
+          <div class="table-column">
+            <div class="flex-row-center">
+              <div class="emoji">➡️</div><div>${ride.pickupLocationName}</div>
+            </div>
+          </div>
+        </div>
+        <div class="table-row">
+          <div class="table-head">Return</div>
+          <div class="table-column">
+            <div class="flex-row-center">
+              <div class="emoji">⬅️</div><div>${ride.returnLocationName}</div>
+            </div>
+          </div>
+        </div>
+        <div class="table-row">
+          <div class="table-head">When</div>
+          <div class="table-column">
           <div class="time-container">
           <div class="hourglass-container">⌛</div>
             <div>
@@ -107,17 +155,22 @@ export const buildHtml = (ride: TransportData) => {
             </div>
           </div>
           </div>
-        </td>
-        </tr>
-        <tr>
-          <th>Car</th>
-          <td>🚘 ${printCarInfo(ride)}</td>
-        </tr>
-      </table>
+        </div>
+        </div>
+        <div class="table-row">
+          <div class="table-head bottom-left-radius" >Car</div>
+          <div class="table-column bottom-right-radius">
+            <div class="flex-row-center">
+            <div class="emoji">🚘</div> ${printCarInfo(ride)}</div>
+            </div>
+        </div>
+      </div>
     </div>
     <br>
     <a href="https://www.hertzfreerider.se/sv-se">Till alla gratisresor</a>
   </body>
   </html>
   `;
+
+  return doc;
 };
