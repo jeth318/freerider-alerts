df4b7039fa0d0f52f72d36e3fb2e8c1576c469ef
jeth318 <jesper.thornberg@me.com>
2024-07-03 08:34:47 +0200

    Fix conflict

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,5 @@
-<<<<<<< Updated upstream
 import { getRideId } from "./utils/ride.util";
 import { getAllSubscriptions, insertRide } from "./db/actions";
-=======
-import {
-  getRideId,
-  getRidesByPickupAndReturnCities,
-  getRidesByPickupCity,
-} from "./utils/ride.util";
-import { insertRide, insertRider, insertSubscription } from "./db/actions";
-import { sendEmail } from "./nodemailer";
->>>>>>> Stashed changes
 import { eHandler } from "./utils/error.util";
 import { fetchRides } from "./resources/hertz.resource";
 import { tick } from "./utils/time.util";
@@ -21,18 +11,7 @@ import {
 export default async () => {
   tick();
   try {
-<<<<<<< Updated upstream
     await addSubscription();
-=======
-    //await insertRider("jesper.thornberg@me.com", "Jesper");
-    await insertSubscription({
-      riderEmail: "jesper.thornberg@me.com",
-      filterType: "from_to",
-      fromCityId: 1,
-      toCityId: 2,
-    });
-
->>>>>>> Stashed changes
     const rides = await fetchRides();
 
     const subscriptions = await getAllSubscriptions();
